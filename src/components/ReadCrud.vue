3276e29cbf818350957bf235fff50e5dfd1486f9
bdquispe <daysi.quispe444@gmail.com>
2022-02-22 08:49:58 -0400

    first commit

---
+++ src/components/ReadCrud.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="contenedor">
+    <div class="contenedor-estudiantes">
+      <div class="contenedor-tabla">
+        <table id="tabla" class="listado-estudiantes">
+          <thead>
+            <tr>
+              <td>nombre</td>
+              <td>paterno</td>
+              <td>materno</td>
+              <td>genero</td>
+              <td>nacionalidad</td>
+              <td>acciones</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="estudiante in datos" :key="estudiante">
+              <td>{{ estudiante.nombre }}</td>
+              <td>{{ estudiante.paterno }}</td>
+              <td>{{ estudiante.materno }}</td>
+              <td>{{ estudiante.genero }}</td>
+              <td>{{ estudiante.nacionalidad }}</td>
+              <td><modificar :id_estudiante="estudiante.id"></modificar></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import datosBD from "@/assets/datos.json"; //include
+import Modificar from "@/components/UpdateCrud.vue";
+export default {
+  data: () => ({
+    datos: datosBD,
+  }),
+  components:{
+    Modificar
+  }
+};
+</script>
+
+<style>
+.contenedor {
+  max-width: 1100px;
+  margin: 0 auto;
+}
+.contenedor-estudiantes {
+  max-width: 800px;
+  margin: 1rem auto 0 auto;
+}
+/* .contenedor-tabla {
+  overflow: scroll;
+} */
+.listado-estudiantes {
+  width: 100%;
+  margin-top: 1rem;
+  border-collapse: collapse;
+}
+.listado-estudiantes thead {
+  background-color: #52be80;
+  color: #2471a3;
+  text-transform: uppercase;
+  font-weight: bold;
+}
+.listado-estudiantes thead th {
+  padding: 1rem 0;
+}
+.listado-estudiantes tbody td {
+  padding: 0.5rem;
+  background-color: #e8f9ff;
+}
+.listado-estudiantes tbody td:nth-child(4) {
+  text-align: center;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h2>Listado de estudiantes</h2>
+    <Read />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Read from "@/components/ReadCrud.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Read,
+  },
+};
+</script>
